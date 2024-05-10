a42e89cd01b7332daf5f85e22c887b33791e5d9a
Philip King <king.phil@gmail.com>
2024-05-09 19:01:11 -0500

    initial versions.tf in hypervisor/ module

---
+++ hypervisor/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  # https://github.com/hashicorp/terraform/releases
+  required_version = ">= 0.13"
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = ">= 0.6.2, < 1.0"
+
+      configuration_aliases = [libvirt]
+    }
+  }
+}
